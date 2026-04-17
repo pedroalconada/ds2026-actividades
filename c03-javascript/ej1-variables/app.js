04fe63ebd00ef0dca685c62d046f3da3b720de4f
pedroalconada <pedritoalconada@gmail.com>
2026-04-17 15:40:21 -0300

    Ejercicio 1 - variables y operaciones

---
+++ c03-javascript/ej1-variables/app.js
@@ -0,0 +1,11 @@
+const nombre = 'Pedro';
+const edad = 19;
+const materia = 'desarrollo de sistemas';
+
+console.log(`Me llamo ${nombre}, tengo ${edad} anios y curso ${materia}`);
+
+let contador = 0;
+for (let i = 0; i < 3; i++) {
+    contador++;
+}
+console.log("Valor final del contador: ", contador);
