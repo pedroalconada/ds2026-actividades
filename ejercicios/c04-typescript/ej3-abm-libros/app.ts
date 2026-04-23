76afc4ed302ed5cd4d229919695c691aecb35fec
pedroalconada <pedritoalconada@gmail.com>
2026-04-23 17:27:53 -0300

    ejercicios - typescript

--- ejercicios/c04-typescript/ej3-abm-libros/app.ts
+++ ejercicios/c04-typescript/ej3-abm-libros/app.ts
@@ -8,7 +8,7 @@ interface Libro {
 }
 
 const catalogo: Libro [] = [
-    {isbn:"123", titulo: "Libro1", autor: "Juan", genero: "", precio: 200, disponible: true},    
-    {isbn:"456", titulo: "Libro2", autor: "Pedro", genero: "", precio: 500, disponible: false},
-    {isbn:"789", titulo: "Libro3", autor: "Simon", genero: "", precio: 1000, disponible: true},
+    {isbn:"123", titulo: "Libro 1", autor: "Juan", genero: "", precio: 200, disponible: true},    
+    {isbn:"456", titulo: "Libro 2", autor: "Pedro", genero: "", precio: 500, disponible: false},
+    {isbn:"789", titulo: "Libro 3", autor: "Simon", genero: "", precio: 1000, disponible: true},
 ]
