c2c6d5cc6472a8fc751911b6355cd137bbeca86c
pedroalconada <pedritoalconada@gmail.com>
2026-04-23 11:44:05 -0300

    Ejercicio 1 - Medio arbol de asteriscos tipados

--- ejercicios/c04-typescript/ej1-medio-arbol/app.js
+++ ejercicios/c04-typescript/ej1-medio-arbol/app.js
@@ -1,15 +1,14 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generarMedioArbol = () => {
-    const numeroAltura = document.querySelector("#numeroAltura").value;
+const medioArbol = () => {
+    const inputAltura = document.querySelector("#numeroAltura");
+    const numAltura = parseInt(inputAltura.value);
     const resultado = document.querySelector("pre");
-    if (parseInt(numeroAltura) < 1) {
+    if (Number(numAltura) < 1) {
         resultado.textContent = "Error: ingrese un numero mayor a 1";
         return;
     }
     let arbol = "";
-    let numero = parseInt(numeroAltura);
-    for (let i = 1; i <= numero; i++) {
+    for (let i = 1; i <= numAltura; i++) {
         arbol += "*".repeat(i) + "\n";
     }
     resultado.textContent = arbol;
--- ejercicios/c04-typescript/ej1-medio-arbol/app.ts
+++ ejercicios/c04-typescript/ej1-medio-arbol/app.ts
@@ -1,15 +1,16 @@
-const generarMedioArbol = (): void => {
-    const numeroAltura: string = (document.querySelector("#numeroAltura") as HTMLInputElement).value;
+const medioArbol = (): number | void => {
+    const inputAltura = (document.querySelector("#numeroAltura") as HTMLInputElement);
+    const numAltura = parseInt(inputAltura.value);
     const resultado = document.querySelector("pre") as HTMLPreElement;
-    if (parseInt(numeroAltura) < 1) {
+
+    if (Number(numAltura) < 1) {
         resultado.textContent = "Error: ingrese un numero mayor a 1";
         return;
     }
 
     let arbol: string = "";
-    let numero: number = parseInt(numeroAltura);
 
-    for (let i: number = 1; i <= numero; i++){
+    for (let i: number = 1; i <= numAltura; i++){
         arbol += "*".repeat(i) + "\n";
     }
 
